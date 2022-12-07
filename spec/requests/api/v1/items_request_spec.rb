6c00c9345cc0e2239e182cefaf3a256034f9a027
Krisetn Nestler <kristen.nestler@gmail.com>
2022-12-07 03:06:23 -0500

    destroy complete

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -83,4 +83,17 @@ describe "Items API" do
     expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq("jeans")
   end
+
+  it "can destroy an item" do
+    item = create(:item)
+    # id = create(:item).id
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
